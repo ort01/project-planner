d60a287b8c4e9c460af3ca03685afe4ce6387fdc
ort01 <ortutova.kristina@gmail.com>
2023-01-10 16:10:16 +0100

    Adding Nav Bar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav class="main-nav">
+    <router-link to="/">Projects</router-link> |
+    <router-link :to="{ name: 'AddProject' }">Add a New Project</router-link>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.main-nav {
+  text-align: center;
+  margin: 40px auto;
+}
+.main-nav a {
+  display: inline-block;
+  text-decoration: none;
+  font-size: 18px;
+  color: #999;
+  margin: 0 10px;
+}
+
+a.router-link-active {
+  border-bottom: 2px solid rgba(69, 148, 69, 0.521);
+  color: rgb(87, 87, 87);
+}
+</style>
--- src/views/AddProject.vue
+++ src/views/AddProject.vue
@@ -10,7 +10,7 @@
 
 <script>
 export default {
-  name: "",
+  name: "AddProject",
   props: [],
   data: () => {
     return {
